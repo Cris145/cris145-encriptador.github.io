ce04c850017b83f90fe023201f6dd8e481ad9dc5
Cristian <cr337766@gmail.com>
2023-05-31 09:07:52 -0500

    Colocado boton de copiar y código arreglado

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,38 @@ let botonEncriptar = document.querySelector(".encriptar");
 let botonDesencriptar = document.querySelector(".desencriptar");
 let imagenesVacias = document.querySelector(".emptyInfo")
 let zonaDeTexto = document.querySelector(".outpuText") 
+let copy_btn = document.querySelector(".copy_btn")
 
-const E = "enter";
+
+const llaveDeEncriptacion = [["a","ai"],["e","enter"],["i","imes"],["o","ober"],["u","ufat"] ]
+
+console.log(llaveDeEncriptacion[1])
+
+const encriptar = (textoEncriptar) =>{
+    let texto = ""
+    for (let i = 0; i < textoEncriptar.length; i++) {
+       if (textoEncriptar.includes(llaveDeEncriptacion[i][0])) {
+            texto = textoEncriptar.replaceAll(llaveDeEncriptacion[i][0], llaveDeEncriptacion[i][1])
+       }
+        
+    }
+};
+
+const desencriptar =(textoDesencriptar) =>{
+    let texto = ""
+    for (let i = 0; i < textoDesencriptar.length; i++) {
+       if (textoDesencriptar.includes(llaveDeEncriptacion[i][1])) {
+            texto = textoDesencriptar.replaceAll(llaveDeEncriptacion[i][1], llaveDeEncriptacion[i][0])
+       }
+        
+    }
+}
+
+
+
+
+
+/*const E = "enter";
 const I = "imes";
 const A = "ai";
 const O = "ober";
@@ -42,12 +72,12 @@ const desencriptar = texto =>{
         textoFinal = textoFinal.replaceAll("ufat", "u");
     }
     return textoFinal
-}
+}*/
 
-console.log(entrada.value)
 function salidaDeTextoEncriptado() {
     imagenesVacias.style.visibility = "hidden" 
     zonaDeTexto.style.visibility = "visible"
+    copy_btn.style.visibility = "visible"
     zonaDeTexto.innerHTML = encriptar(entrada.value)
 }
 
@@ -55,7 +85,18 @@ function salidaDeTextoEncriptado() {
 function salidaDeTextoDesencriptado() {
     imagenesVacias.style.visibility = "hidden" 
     zonaDeTexto.style.visibility = "visible"
+    copy_btn.style.visibility = "visible"
     zonaDeTexto.innerHTML = desencriptar(entrada.value)
 }
 botonEncriptar.addEventListener("click", salidaDeTextoEncriptado);
 botonDesencriptar.addEventListener("click",salidaDeTextoDesencriptado);
+copy_btn.addEventListener("click",copiarEnPapelera);
+
+function copiarEnPapelera() {
+    zonaDeTexto.select()
+    zonaDeTexto.setSelectionRange(0, 99999); 
+
+    navigator.clipboard.writeText(zonaDeTexto.value);
+    zonaDeTexto.value =""
+}
+
